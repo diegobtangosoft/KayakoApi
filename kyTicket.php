c0e308d4afb0e7a09a3c6c4bb2e0796e94543dd3
Lucas Ricardo Wojtun <lucasw@axoft.com>
2026-09-18 16:04:37 -0300

    validacion de dato bccemails ?? ''

--- kyTicket.php
+++ kyTicket.php
@@ -521,7 +521,7 @@ class kyTicket extends kyObjectWithCustomFieldsBase
 		$this->owner_staff_name = $data['ownerstaffname'];
 		$this->full_name = $data['fullname'];
 		$this->email = $data['email'];
-		$this->bccemails = $data['bccemails'];
+		$this->bccemails = $data['bccemails'] ?? '';
 		$this->last_replier = $data['lastreplier'];
 		$this->subject = $data['subject'];
 		$this->creation_time = ky_assure_positive_int($data['creationtime']);
